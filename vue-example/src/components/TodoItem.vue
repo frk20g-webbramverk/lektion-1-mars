8c185aa7e2928f9345c0363f5b7e48addd312aa9
Christoffer Wallenberg <christoffer.wallenberg@zocom.se>
2021-03-01 10:37:00 +0100

    Added code

---
+++ vue-example/src/components/TodoItem.vue
@@ -0,0 +1,26 @@
+<template>
+  <li v-on:click="update">{{ task }}</li>
+</template>
+
+<script>
+export default {
+    name: 'todo-item',
+    props: {
+        todo: Object
+    },
+    computed: {
+        task() {
+            return 'Task: ' + this.todo.task + ' Done: ' + this.todo.done ;
+        }
+    },
+    methods: {
+        update: function() {
+            this.$emit('done', this.task);
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
